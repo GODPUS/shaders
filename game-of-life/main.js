b7c164d41e757774e43c8eb144131458808290bd
GODPUS <mikeparisi@gmail.com>
2014-06-09 18:13:16 -0700

    trying to get resizing to work

--- game-of-life/main.js
+++ game-of-life/main.js
@@ -1,36 +1,25 @@
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
-var RESOLUTION = 1024;
 var MOUSE = { x: 0, y: 0 };
 var CLOCK = new THREE.Clock();
-
-document.addEventListener('mousemove', function(e){ 
-    MOUSE.x = e.clientX || e.pageX; 
-    MOUSE.y = e.clientY || e.pageY 
-}, false);
 
 var basicVertexShader = document.getElementById('shader-vs').text;
 var simFragmentShader = document.getElementById('shader-gol-fs').text;
 
-var simBuffer =  new THREE.WebGLRenderTarget(RESOLUTION, RESOLUTION, { minFilter: THREE.NearestFilter, magFilter: THREE.LinearMipMapLinearFilter, format: THREE.RGBAFormat } );
-var backBuffer = new THREE.WebGLRenderTarget(RESOLUTION, RESOLUTION, { minFilter: THREE.NearestFilter, magFilter: THREE.LinearMipMapLinearFilter, format: THREE.RGBAFormat } );
-
-var simScene = new THREE.Scene();
-var displayScene = new THREE.Scene();
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(RESOLUTION, RESOLUTION);
-document.body.appendChild(renderer.domElement);
-renderer.autoClear = false;
-
+var simBuffer =  new THREE.WebGLRenderTarget(WIDTH, HEIGHT, { minFilter: THREE.NearestFilter, magFilter: THREE.NearestFilter, format: THREE.RGBAFormat } );
+var backBuffer = new THREE.WebGLRenderTarget(WIDTH, HEIGHT, { minFilter: THREE.NearestFilter, magFilter: THREE.NearestFilter, format: THREE.RGBAFormat } );
 
 var simUniforms = { 
 	"backbuffer" : { type: "t",  value: backBuffer },
-	"resolution" : { type: "v2", value: new THREE.Vector2(RESOLUTION, RESOLUTION) },
+	"resolution" : { type: "v2", value: new THREE.Vector2(WIDTH, HEIGHT) },
 	"iteration"  : { type: "i",  value: 0 },
 	"time"       : { type: "f",  value: 0.0 },
 	"mouse"      : { type: "v2", value: new THREE.Vector2(MOUSE.x, MOUSE.y) }
 };
+
+
+var simScene = new THREE.Scene();
+var displayScene = new THREE.Scene();
 
 var simQuad = new THREE.Mesh(new THREE.PlaneGeometry(2,2,0), new THREE.ShaderMaterial({ uniforms: simUniforms, vertexShader: basicVertexShader, fragmentShader: simFragmentShader}));
 simScene.add(simQuad);
@@ -41,6 +30,10 @@ displayScene.add(outQuad);
 displayScene.add(camera);
 
 var camera = new THREE.Camera();
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(WIDTH, HEIGHT);
+document.body.appendChild(renderer.domElement);
+renderer.autoClear = false;
 var bufferState = 0;
 
 function render() {
@@ -56,10 +49,22 @@ function render() {
 
    simUniforms.iteration.value += 1;
    simUniforms.time.value += CLOCK.getDelta();
-   simUniforms.mouse.value = new THREE.Vector2(MOUSE.x, MOUSE.y);
+   simUniforms.mouse.value = new THREE.Vector2(MOUSE.x/WIDTH, (HEIGHT-MOUSE.y)/HEIGHT);
 
    renderer.render(displayScene, camera);
    requestAnimationFrame(render);
 }
 
 window.onload = function(){ requestAnimationFrame(render); }
+window.onresize = function(){ 
+	WIDTH = window.innerWidth;
+	HEIGHT = window.innerHeight;
+	renderer.setSize(WIDTH, HEIGHT);
+	renderer.setViewport(0, 0, WIDTH, HEIGHT);
+	renderer.clear(false);
+}
+
+document.addEventListener('mousemove', function(e){ 
+    MOUSE.x = e.clientX || e.pageX; 
+    MOUSE.y = e.clientY || e.pageY;
+}, false);
